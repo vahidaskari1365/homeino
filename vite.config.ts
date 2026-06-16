97d4a618b72dc8480d3385a7c73b84c4b0958ad9
vahidaskari1365 <vahid.askari1986@gmail.com>
2026-06-16 10:55:32 +0000

    🐛 fix: add base path and disable sourcemaps

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,7 @@ import { VitePWA } from "vite-plugin-pwa";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: "/",
   server: {
     host: "::",
     port: 8080,
@@ -15,6 +16,7 @@ export default defineConfig(({ mode }) => ({
   },
   build: {
     chunkSizeWarningLimit: 2000,
+    sourcemap: false,
   },
   plugins: [
     react(),
